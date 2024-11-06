e4c517b49a490eafca718c075a74829eb792dc6a
William Takahashi <william.ryort@gmail.com>
2024-11-06 04:28:13 -0300

    first commit

---
+++ Controllers/OfferController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Unite.Controllers
+{
+    [Route("api/offers")]
+    [ApiController]
+    public class OfferController : ControllerBase
+    {
+        private readonly ILogger<OfferController> _logger;
+
+        public OfferController(ILogger<OfferController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult GetPaginatedOffers()
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetOfferById([FromRoute] string offerId)
+        {
+            return Ok(offerId);
+        }
+    }
+}
